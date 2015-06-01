cb0ed971f3fe98b3849121157d1b56825dd5b6b9
Henry Wong <henryw4k@gmail.com>
2015-05-31 19:56:12 -0700

    change GET request from http to https

--- angularApp.js
+++ angularApp.js
@@ -31,7 +31,7 @@ app.factory('wordNik', ['$http',
 	function($http){
 		var obj = {};
 		obj.getWordPromise = function(){
-		 return $http.get("http://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
+		 return $http.get("https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
 	     };//getWord
 
 	    obj.shuffle = function(string){
