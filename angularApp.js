946553c40fa4cfdd6522a4a286b3ee345d42cedc
Henry Wong <henryw4k@gmail.com>
2015-06-05 17:32:53 -0700

    (feat) finished back button

--- angularApp.js
+++ angularApp.js
@@ -1,25 +1,52 @@
 var app = angular.module('Scrabble', [])
 
 app.controller('MainCtrl', [
-'$scope', 'wordNik', '$filter',
-function($scope, wordNik, $filter){
+'$scope', 'wordNik', '$filter','$document',
+function($scope, wordNik, $filter, $document){
   $scope.guess = '';
 
   wordNik.getWordPromise()
   .then(function(response){
   	$scope.word = response.data.word.toUpperCase();
   	$scope.shuffled = wordNik.shuffle($scope.word);
- 	  $scope.$on('my:keyup', function(event, keyEvent) {
- 	var shuffled = $scope.shuffled;
-    var keyTyped = $filter('convert')(keyEvent.keyCode);
-	$scope.shuffled = wordNik.checkHelper(keyTyped, shuffled);
-	if($scope.shuffled.length !== shuffled.length){
-		$scope.guess = $scope.guess + keyTyped;
-	}
-  });
+    //storeLeftOver in wordNik
+
+    $scope.$on('my:keyup', function(event, keyEvent) {
+      var keyTyped = $filter('convert')(keyEvent.keyCode);
+      var tempShuffled = wordNik.checkHelper(keyTyped, $scope.shuffled);
+      //tempShuffled has value if key is a match
+      if( tempShuffled && $scope.guess.length <= $scope.word.length){
+
+        //model updates
+        $scope.shuffled = wordNik.scramble;
+        $scope.guess = wordNik.guess;
+        //winning logic
+        if(new String($scope.word).valueOf() === new String($scope.guess).valueOf()){
+          console.log("YOU WIN!!!!!");
+          window.alert("You Win!");
+        }
+      }
+    });
+      console.log("shuffled word: ", wordNik.scramble);
+      console.log("guess word: ", wordNik.guess);
+      console.log("stack: ", wordNik.stack);
+
   });
 
-}]);
+  //Back space logic
+  $document.on('keydown', function(e){
+    if(e.which === 8){
+        e.preventDefault();
+          wordNik.backHelper();
+
+          //model updates
+          console.log(' back key: ', wordNik.scramble);
+          $scope.shuffled = wordNik.scramble;
+          $scope.guess = wordNik.guess;
+    }
+  });
+
+}]);//controller logic
 
 app.filter('convert', function(){
   return function(keycode){
@@ -30,7 +57,22 @@ app.filter('convert', function(){
 app.factory('wordNik', ['$http',
 	function($http){
 		var obj = {};
+
+    //persistent memory
+    obj.scramble = '';
+    obj.guess = '';
+    obj.stack = []; //a stack {character, index}
+    //Setter methods
+    obj.storeLeftOver = function(val){
+      this.scramble = val;
+    };
+
+    obj.storeGuess = function(val){
+      this.guess = val;
+    }
+
 		obj.getWordPromise = function(){
+      //TODO: making strings come without a '-'
 		 return $http.get("https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5");
 	     };//getWord
 
@@ -50,14 +92,30 @@ app.factory('wordNik', ['$http',
 	    obj.checkHelper = function(character, shuffle){
 	    	for(var i = 0; i < shuffle.length; i++){
 	    		if(character === shuffle[i]){
+            this.stack.push({'character': character,'index': i});
 	    			var left = shuffle.substring(0,i);
 	    			var right = shuffle.substring(i+1);
-	    			console.log(left+right);
-	    			return left+right;
+            var newShuffle = left+right;
+
+            this.scramble = newShuffle;
+            this.guess = this.guess + character;
+	    			return true;
 	    		}
 	    	}
-	    	return shuffle;
+	    	return false;
 	    };
+      obj.backHelper = function(){
+        if(this.stack.length > 0){
+          var lastItem = this.stack.pop();//need index location
+          
+          this.guess = this.guess.substring(0, this.guess.length-1);
+          //update guess & scramble local variables
+          var left = this.scramble.substring(0,lastItem.index);
+          var right = this.scramble.substring(lastItem.index);
+          var newValue = left + lastItem.character + right;
+          this.scramble = newValue;
+        }
+      }
 
 	     return obj;
 }]);
